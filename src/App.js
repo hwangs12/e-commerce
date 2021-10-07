f67248b298a440474c75da065ff6e2dab79c0731
hwangs12 <sungjun94@gmail.com>
2021-10-06 21:32:26 -0400

    setting route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
+import { AboutPage, HomePage, ProductsPage, ErrorPage } from "./pages/index";
+
 function App() {
 	return (
 		<Router>
+			<Navbar />
+			<Sidebar />
+			<Footer />
 			<Switch>
 				<Route path="/" exact>
-					<Navbar />
-					<Sidebar />
-					<Footer />
+					<HomePage />
+				</Route>
+				<Route path="/about">
+					<AboutPage />
+				</Route>
+				<Route path="/products">
+					<ProductsPage />
+				</Route>
+				<Route path="*">
+					<ErrorPage />
 				</Route>
 			</Switch>
 		</Router>

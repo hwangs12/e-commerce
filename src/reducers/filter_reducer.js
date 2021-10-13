03bd537be86a1a1d70e45be762a4296db6e2b324
hwangs12 <sungjun94@gmail.com>
2021-10-13 12:30:26 -0400

    added sort feature complete

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -38,6 +38,30 @@ const filter_reducer = (state, action) => {
 			...action.payload,
 		};
 	}
+
+	if (action.type === SORT_PRODUCTS) {
+		const { filtered_products: products, sort } = state;
+		let temp_products = [...products];
+
+		if (sort === "price-lowest") {
+			temp_products = temp_products.sort((a, b) => a.price - b.price);
+		}
+		if (sort === "price-highest") {
+			temp_products = temp_products.sort((a, b) => b.price - a.price);
+		}
+		if (sort === "name-a") {
+			temp_products = temp_products.sort((a, b) =>
+				a.name.localeCompare(b.name)
+			);
+		}
+		if (sort === "name-z") {
+			temp_products = temp_products.sort((a, b) =>
+				b.name.localeCompare(a.name)
+			);
+		}
+
+		return { ...state, filtered_products: temp_products };
+	}
 	throw new Error(`No Matching "${action.type}" - action type`);
 };
 

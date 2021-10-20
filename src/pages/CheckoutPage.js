f15658774acd9ab54342bc2545f3db56696bfd61
hwangs12 <sungjun94@gmail.com>
2021-10-20 08:47:36 -0400

    adding checkout page - work in progress

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -6,12 +6,31 @@ import { useCartContext } from "../context/cart_context";
 import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
+	const { cart } = useCartContext();
 	return (
 		<main>
 			<PageHero text="checkout" />
-			<Wrapper className="page"></Wrapper>
+			<Wrapper className="page">
+				{cart.length < 1 ? (
+					<div className="empty">
+						<h2>your cart is empty</h2>
+						<Link to="/products" className="btn">
+							fill it
+						</Link>
+					</div>
+				) : (
+					<StripeCheckout />
+				)}
+			</Wrapper>
 		</main>
 	);
 };
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	.empty {
+		text-align: center;
+	}
+`;
 export default CheckoutPage;
